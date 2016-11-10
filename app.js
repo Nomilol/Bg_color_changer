2f0d4ea6d428ec887667b1f086af8a06ecfd22fc
Axel <lezinaxel@gmail.com>
2016-11-10 11:33:46 +0100

    recup des input + label

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
 console.log('start');
+
+(function(){
+	var app = {
+		init : function(){
+			app.recupColorInput();
+		},
+		recupColorInput : function(){
+			redValue = $('#red').val();
+			greenValue = $('#green').val();
+			blueValue = $('#blue').val();
+			console.log(redValue, greenValue, blueValue);
+		}
+	}
+
+
+
+
+
+
+
+
+
+	app.init();
+})();
