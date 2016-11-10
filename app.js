ff62af08e9b9844541a8334bdc1bcb9c6790ad47
Axel <lezinaxel@gmail.com>
2016-11-10 14:02:59 +0100

    L'appli marche bien , la couleur du fond change bien en changeant les curseurs rouge , vert et bleu

--- app.js
+++ app.js
@@ -5,42 +5,47 @@ console.log('start');
 
 		init : function(){
 			app.recupValueInput();
-
+			app.listeners();
+			
+		},
+		listeners : function(){
+			$('.color').on('input',app.recupValueInput);
+			
+			
 		},
 		recupValueInput : function(){
-			redValue = $('#red').val();
-			greenValue = $('#green').val();
-			blueValue = $('#blue').val();
-			app.valueInputGreen();
-			app.valueInputRed();
-			app.valueInputBlue();
+			var redValue = $('#red').val();
+			var greenValue = $('#green').val();
+			var blueValue = $('#blue').val();
+			$('body').css('backgroundColor','rgb('+redValue+', '+greenValue+','+blueValue+')');
 			console.log(redValue, greenValue, blueValue);
+			app.valueInput();
 
 		},
 		changeBodyColor : function(){
 			
 		},
-		valueInputRed : function(){
+		valueInput : function(){
 			$('#red').on('input', function(){
 				$(this).next().text('--');
 				var $set = $(this).val();
 				$(this).next().text($set);
 			});
 
-		},
-		valueInputGreen : function(){
 			$('#green').on('input', function(){
 				$(this).next().text('--');
 				var $set = $(this).val();
 				$(this).next().text($set);
 			});
-		},
-		valueInputBlue : function(){
+
 			$('#blue').on('input', function(){
 				$(this).next().text('--');
 				var $set = $(this).val();
 				$(this).next().text($set);
 			});
+		},
+		changeColor : function(){
+
 		}
 	}
 
