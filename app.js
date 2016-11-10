f39bd8cce700e26372ab5fa9be23a87bfe97168a
Axel <lezinaxel@gmail.com>
2016-11-10 12:05:35 +0100

    Chiffre qui affiche la position du curseur

--- app.js
+++ app.js
@@ -2,14 +2,45 @@ console.log('start');
 
 (function(){
 	var app = {
+
 		init : function(){
-			app.recupColorInput();
+			app.recupValueInput();
+
 		},
-		recupColorInput : function(){
+		recupValueInput : function(){
 			redValue = $('#red').val();
 			greenValue = $('#green').val();
 			blueValue = $('#blue').val();
+			app.valueInputGreen();
+			app.valueInputRed();
+			app.valueInputBlue();
 			console.log(redValue, greenValue, blueValue);
+
+		},
+		changeBodyColor : function(){
+			
+		},
+		valueInputRed : function(){
+			$('#red').on('input', function(){
+				$(this).next().text('--');
+				var $set = $(this).val();
+				$(this).next().text($set);
+			});
+
+		},
+		valueInputGreen : function(){
+			$('#green').on('input', function(){
+				$(this).next().text('--');
+				var $set = $(this).val();
+				$(this).next().text($set);
+			});
+		},
+		valueInputBlue : function(){
+			$('#blue').on('input', function(){
+				$(this).next().text('--');
+				var $set = $(this).val();
+				$(this).next().text($set);
+			});
 		}
 	}
 
